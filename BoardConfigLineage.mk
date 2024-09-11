cca743ea3dca8fec2ebeaf4eab6e852ed291d0a5
Ionut Gherman <ghermanionut96@gmail.com>
2024-09-11 16:08:23 +0300

    zuma: enable BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,6 +5,7 @@
 #
 
 DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml

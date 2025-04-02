5819a07197070c8e2e2ab1bee4040f301df1cfa2
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-04-02 17:26:12 -0400

    Partial bring-up for almost all custom ROMs

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -8,7 +8,11 @@ DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Manifests
+ifdef ROM_VENDOR
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/$(ROM_VENDOR)/config/device_framework_matrix.xml
+else
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+endif
 DEVICE_MANIFEST_FILE += \
     device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
     device/google/zuma/manifest_radio_ds.xml

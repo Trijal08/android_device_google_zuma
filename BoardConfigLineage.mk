64c6500cfae485bfacdfb103e35fbf3cfb12649e
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-04-02 17:26:12 -0400

    Partial bring-up for almost all custom ROMs

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -12,7 +12,11 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
+ifdef ROM_VENDOR
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/$(ROM_VENDOR)/config/device_framework_matrix.xml
+else
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+endif
 DEVICE_MANIFEST_FILE += \
     device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
     device/google/zuma/manifest_radio_ds.xml
